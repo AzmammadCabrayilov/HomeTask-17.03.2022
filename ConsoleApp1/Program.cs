36d88c78966b8762428aebfa3a7bcf662c134a33
AzmammadCabrayilov <7c0dj00@code.edu.az>
2022-03-17 01:25:49 +0400

    MethodArea

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine(Area(5));
+
+            //Console.WriteLine(Area(5, 6));
+
+            //Console.WriteLine(Area(1, 2, 3));
+        }
+
+        //Area is Circle
+        static int Area(int radius)
+        {
+            int p = 3;
+            int area = p * (radius * radius);
+            return area;
+        }
+
+        //Area is Square
+        static int Area(int a, int b)
+        {
+            int area = a * b;
+            return area;
+        }
+
+        //Area is Square Paralelepiped
+        static int Area(int a, int b, int c)
+        {
+            int area = a * b + a * c + b * c;
+            return area;
+        }
+
+    }
+}
